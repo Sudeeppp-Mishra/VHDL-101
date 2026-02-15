c561b1c376858f2fee857042d9b8f9e8298c602a
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2026-02-15 14:43:47 +0545

    feat: add 2x1 mux vhdl for structural modeling

---
+++ Structural-Modeling/4x1-Mux/MUX_2x1.vhd
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity MUX_2x1 is
+    port(
+        I0, I1 : in std_logic;
+        S : in std_logic;
+        Y : out std_logic
+    );
+end MUX_2x1;
+
+architecture Behavioral of MUX_2x1 is
+begin
+    process(I0, I1, S)
+    begin
+        case S is
+            when '0' => Y <= I0;
+            when '1' => Y <= I1;
+            when others => Y <= '0';
+        end case;
+    end process;
+end Behavioral;
